c46276e9c6a632ae69400383521fe0d9c60c70ae
Echonamad <tmechonamad@gmail.com>
2026-06-18 20:54:04 +0200

    Update inhalte.js

--- inhalte.js
+++ inhalte.js
@@ -14,7 +14,7 @@
 const inhalte = {
 
   // ---- Kopfbereich / Eyebrow-Text über dem Produktnamen ----
-  eyebrow: "SMARTSTORE JenaSvenEleven · PRODUKTINFO",
+  eyebrow: "SMARTSTORE JenaSevenEleven · PRODUKTINFO",
 
   // ---- Produktname und Untertitel ----
   produktname: "L'Oréal Men Expert Barber Club",
